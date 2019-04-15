9a1e04e1abda1884b160da0b04bb973637ba7a04
Manoj Patil <Q3479@Corp.qualitykiosk.com>
2019-04-15 10:24:36 +0530

    test01

---
+++ src/Test1/PopUp.java
@@ -0,0 +1,19 @@
+package Test1;
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class PopUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("hi this is test message");
+			System.out.println("Just for the commit");
+		
+	}
+
+}
